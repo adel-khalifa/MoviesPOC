572a2082e63ee6e3f05fa64f26b026cd776939ee
Adel El-Hawary <adeltarek.088@gmail.com>
2022-11-09 20:51:16 +0200

    implemented ui -relocate some Composable

--- app/src/main/java/com/adel/moviespoc/ui/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/adel/moviespoc/ui/screens/details/DetailsScreen.kt
@@ -1,30 +1,26 @@
 package com.adel.moviespoc.ui.screens.details
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.adel.moviespoc.R
 import com.adel.moviespoc.data.models.MovieId
-import com.adel.moviespoc.ui.components.LoadingComponent
-import com.adel.moviespoc.ui.components.RemoteImage
+import com.adel.moviespoc.ui.components.*
 import com.adel.moviespoc.ui.screens.list.RenderErrorState
 import com.adel.moviespoc.ui.screens.util.extractErrorMessage
 import com.google.accompanist.swiperefresh.SwipeRefresh
@@ -161,94 +157,3 @@ fun RenderMovieDetails(
 
 
 }
-
-@Composable
-fun RateSection(vote: Double) {
-    Row(
-        Modifier,
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically
-
-    ) {
-
-
-        Image(
-            modifier = Modifier
-                .wrapContentSize()
-                .size(20.dp),
-            painter = painterResource(id = R.drawable.round_star_rate_24),
-            colorFilter = ColorFilter.tint(Color.Yellow),
-            contentDescription = ""
-        )
-        Spacer(modifier = Modifier.width(2.dp))
-        CustomText(
-            text = "${vote.toInt()}/10", color = MaterialTheme.colors.onBackground,
-            textSize = 10.sp
-        )
-
-    }
-}
-
-@Composable
-fun TextChip(text: String) {
-    Button(
-        modifier = Modifier.height(32.dp),
-        shape = RoundedCornerShape(corner = CornerSize(18.dp)),
-        elevation = ButtonDefaults.elevation(
-            defaultElevation = 0.dp,
-            pressedElevation = 0.dp,
-            disabledElevation = 0.dp
-        ),
-        border = BorderStroke(.5.dp, MaterialTheme.colors.primary),
-        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 0.dp),
-        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
-        onClick = { }
-    ) {
-        CustomText(text = text, textSize = 14.sp)
-
-    }
-}
-
-@Composable
-fun CustomText(
-    text: String,
-    color: Color? = null,
-    textSize: TextUnit,
-    maxLines: Int = 1
-) {
-    Text(
-        text = text,
-        maxLines = maxLines,
-        overflow = TextOverflow.Ellipsis,
-        modifier = Modifier.wrapContentWidth(Alignment.Start),
-        style = MaterialTheme.typography.h6.copy(
-            color = color ?: MaterialTheme.colors.onBackground,
-            fontWeight = FontWeight.Bold,
-            fontSize = textSize
-        )
-    )
-}
-
-@Composable
-fun ExpandableText(text: String, color: Color? = null) {
-    var isExpanded by remember { mutableStateOf(false) }
-
-    Text(
-        text = text,
-        maxLines = if (isExpanded) 24 else 2,
-        overflow = TextOverflow.Ellipsis,
-        modifier = Modifier
-            .animateContentSize()
-            .clickable { isExpanded = !isExpanded },
-        style = MaterialTheme.typography.body2.copy(
-            color = color ?: MaterialTheme.colors.onBackground,
-            fontWeight = FontWeight.SemiBold,
-        )
-    )
-    AnimatedVisibility(!isExpanded) {
-        CustomText(text = stringResource(id = R.string.show_more), textSize = 12.sp)
-    }
-
-
-}
-
--- app/src/main/java/com/adel/moviespoc/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/adel/moviespoc/ui/screens/list/ListScreen.kt
@@ -1,28 +1,25 @@
-@file:OptIn(ExperimentalFoundationApi::class)
-
 package com.adel.moviespoc.ui.screens.list
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Surface
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.adel.moviespoc.data.models.ErrorMessage
-import com.adel.moviespoc.domain.entities.Movie
 import com.adel.moviespoc.ui.components.ErrorView
 import com.adel.moviespoc.ui.components.LoadingComponent
+import com.adel.moviespoc.ui.components.MovieItem
 import com.adel.moviespoc.ui.components.PaginatedLazyColumn
-import com.adel.moviespoc.ui.components.RemoteImage
-import com.adel.moviespoc.ui.screens.details.CustomText
-import com.adel.moviespoc.ui.screens.details.RateSection
 import com.adel.moviespoc.ui.screens.util.extractErrorMessage
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
@@ -75,6 +72,7 @@ fun RenderErrorState(message: ErrorMessage?, onTryAgain: () -> Unit) {
     ErrorView(LocalContext.current.extractErrorMessage(message), onTryAgain = onTryAgain)
 }
 
+
 @Composable
 fun RenderSuccessState(
     uiState: ListScreenState.Success,
@@ -103,32 +101,4 @@ fun RenderSuccessState(
         }
     }
 
-}
-
-@Composable
-fun MovieItem(modifier: Modifier = Modifier, movie: Movie, onMovieClicked: (id: Int) -> Unit) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .clickable { onMovieClicked(movie.id) }
-            .padding(horizontal = 4.dp),
-        verticalAlignment = Alignment.Top
-    ) {
-
-        RemoteImage(
-            url = movie.posterUrl,
-            modifier = Modifier.size(80.dp)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Column(
-            verticalArrangement = Arrangement.SpaceAround,
-        ) {
-            CustomText(text = movie.title, textSize = 16.sp)
-            Spacer(modifier = Modifier.height(4.dp))
-
-            CustomText(text = movie.overview, textSize = 12.sp, maxLines = 2)
-            RateSection(vote = movie.vote)
-
-        }
-    }
 }
